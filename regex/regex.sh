9e33431fc3ca1b71cf1e5297d73ba1e8d784e110
Jeffrey Miller <mill7707@umm9020-h2ytx12.morris.umn.edu>
2021-09-07 11:19:56 -0500

    Completes the regex exercise, adds the neccessary sed commands to modify the text files.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 
 
-#modifies the r0_input and stores it in new output file
+#Modifies the r0_input and stores it in new output file
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' r0_input.txt > r0_output.txt
+
+#Modifies the r1_input file and converts it into a new text file, ignores the third line
+sed -E -n 's/\* I am ([A-Z]\w+). My favorite sandwich is ([a-z]\w+)./1. \1\n2. \2\n/p' r1_input.txt > r1_output.txt
+
+#Modifies the r2_input file and converts it into a new file.
+sed -E 's/\* sandwich with ([a-zA-Z]\w+.+) (for here|to go)/1. \1\n2. \2\n/' r2_input.txt > r2_output.txt
 
 
